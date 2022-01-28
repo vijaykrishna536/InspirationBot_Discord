7aeca4184e09533defe6938b974d1932acb00417
vijaykrishna536 <30413582+vijaykrishna536@users.noreply.github.com>
2022-01-28 19:45:53 +0000

    Added basic bot

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import discord
+import requests
+import json
+
+my_secret = os.environ['token']
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random/")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+  print('We have logged in as the {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user: 
+    return
+
+  if message.content.startswith('$inspire'):
+    quote = get_quote()
+    await message.channel.send(quote)
+
+client.run(my_secret)
